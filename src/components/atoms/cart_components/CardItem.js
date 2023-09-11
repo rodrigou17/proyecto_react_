224fcaa1601f9db75f078e068cee9553ddc48a6e
rodrigou17 <140988712+rodrigou17@users.noreply.github.com>
2023-09-10 21:05:47 -0300

    avances en estilos del carrito de compras y GRID

--- src/components/atoms/cart_components/CardItem.js
+++ src/components/atoms/cart_components/CardItem.js
@@ -1,18 +1,50 @@
+import { title } from "process"
+import styled from 'styled-components'
 
 
 
 const CartItem = ({data, deleteFromCart}) => {
 
-    const {name, price, id, quantity} = data
+    const {price, title, img, id, quantity} = data
 
     return (
         <div className="cart-item">
-            <h4>{name}</h4>
-            <h5>$ {price} x {quantity}= $ {price * quantity}</h5>
+            <CardContainer>
+            <Image src= {img} alt='' />
+            <figcaption>
+                <CardTitle>{title}</CardTitle>
+            </figcaption>
+            <CardText>$ {price} x {quantity}= $ {price * quantity}</CardText>
             <button onClick={() => deleteFromCart(id)}>Eliminar uno</button>
             <button onClick={() => deleteFromCart(id, true)}>Eliminar todos</button>
+            </CardContainer>
         </div>
     )
 }
 
 export default CartItem
+
+const CardContainer = styled.figure`
+  width: 270px;
+  height: 400px;
+  text-align: left;
+`
+
+const Image = styled.img`
+  width: 100%;
+  height: 70%;
+  border: 1.8px solid black;
+  border-radius: 1.3rem;
+  object-fit: contain;
+`
+
+const CardTitle = styled.h3`
+  font-family: arial; 
+`
+
+const CardText = styled.p`
+  font-size: 1.1rem;
+  font-family: arial;
+  color: gray; 
+  margin-bottom: 0.8rem;
+`
